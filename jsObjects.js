84c8d69d090d3abc67d35c57f3097cb98a516480
eman <emanhashim42@gmail.com>
2025-03-26 10:14:09 +0300

    the two object types added

--- jsObjects.js
+++ jsObjects.js
@@ -40,6 +40,57 @@ console.log(eman.height);
 
  console.log(`${sara.fnn} ${sara.lnn} ${sara.height} ${sara.weigth}`)
 
+
+ //empty js object add prorties later
+
+ const persons = {}
+ persons.firstname = "susu";
+ persons.lastname = "robel";
+ persons.age = 28;
+
+ console.log(`${persons.firstname} ${persons.age}`)
+
+
+//this will create a js object with new keyword
+
+const cat = new Object();
+cat.name = "alice";
+cat.age = 2;
+
+console.log(`${cat.name} ${cat.age}`)
+
+
+
+/**
+ * Object Constructor Functions
+Sometimes we need to create many objects of the same type.
+
+To create an object type we use an object constructor function.
+
+It is considered good practice to name constructor functions with an upper-case first letter.
+ */
+
+
+//this is a constructor function
+function Artist(first, last, age, sex){
+  this.first = first;
+  this.last = last;
+  this.age=age;
+  this.sex=sex;
+
+}
+
+//now create an instance of Artist, we can create as many obejct as we want
+const Art = new Artist("nunu","lala",6 , "Female")
+
+const hi = new Artist("kel", "kuku", 56, "male")
+
+console.log(`${Art.first} ${Art.last} ${Art.age} ${Art.sex}`)
+
+console.log(`${hi.first} ${hi.last}, ${hi.age}, ${hi.sex}`)
+
+
+
 //the other way to do an object is using a new keyword to it
 
 const mom = new Object();
